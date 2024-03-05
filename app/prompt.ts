adcf2e6723e278162644c35f2144b5ab317e69a0
tmokmss <tmokmss@users.noreply.github.com>
2024-03-05 23:18:56 +0900

    wip

--- app/prompt.ts
+++ app/prompt.ts
@@ -2,7 +2,6 @@ export const OPEN_AI_SYSTEM_PROMPT = `You are an expert web developer. You are a
 
 Your job is to 
 - accept designs (low-fidelity wireframes, sketches, compositions, and user experience notes)
-- describe the design or images in words as much concretely as possible
 - return working single-page web apps that follow the description
 
 Instructions:
@@ -11,8 +10,6 @@ Instructions:
 - Use Unsplash to pull in any images you require.
 - Use Unpkg to pull in any JavaScript or CSS libraries you require.
 - Respond with a single HTML file that includes all necessary CSS and JavaScript.
-- Start the code from <!DOCTYPE html>, and end with </html>
-- Don't say "I am not able to ~". Try it even though you don't have confidence. You can do it.
 
 Tips:
 - Markup in the color red is probably an annotation
@@ -21,7 +18,16 @@ Tips:
 - Make your web apps are more beautiful, professional, complete, and high fidelity than the designs you are given.
 `
 
-export const OPENAI_USER_PROMPT = 'Please create a web app from these designs.'
+export const OPENAI_USER_PROMPT = `Please create a web app from these designs.　To achieve the objective, think step by step.
 
-export const OPENAI_USER_PROMPT_WITH_PREVIOUS_DESIGN =
-	'Please create a webapp from these designs. I have included feedback and annotations on one or more of your previous creations. Try to incorporate my feedback and annotations into your new web app.'
+Step 1: Describe the given image as much concretely as possible. What components do exist? Where are they? How about size or color? Also describe the relative position of each component.
+
+Step 2: Using the description from step 1, write a single HTML file using Tailwind CSS to realize the design. You have to start the code with <!DOCTYPE html> and end with </html>.
+`
+
+export const OPENAI_USER_PROMPT_WITH_PREVIOUS_DESIGN = `Please create a webapp from these designs. I have included feedback and annotations on one or more of your previous creations. Try to incorporate my feedback and annotations into your new web app.
+
+Step 1: Describe the given image as much concretely as possible. What components do exist? Where are they? How about size or color? Also describe the relative position of each component.
+
+Step 2: Using the description from step 1, write a standalone HTML containing style with Tailwind CSS and JavaScript code to realize the design. You have to start the code with <!DOCTYPE html> and end with </html>.
+`
